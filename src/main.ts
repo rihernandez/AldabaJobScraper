e73899953367ac20fd66c27c2cc64a5663d7cee1
ykvbnr <ykvbnr@gmail.com>
2025-07-07 10:08:24 -0400

    refactor: adding port to deploy on render

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  const port = process.env.PORT || 5000;
+  const port = process.env.PORT || 10000;
   await app.listen(port, '0.0.0.0');
   
   logger.log(`🚀 Application is running on: http://0.0.0.0:${port}`);
